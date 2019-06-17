2fe957534c473a5994795417e541a774cfbf9d00
Lucas Matos <matoslucas@hotmail.com>
2019-06-16 23:56:55 -0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, } from "react-router-dom";
 
-import Carousel from "./com/Carousel"
+import Carousel from "./com/Carousel";
 
 function App() {
-
   return (
-    <Carousel />
+    <Router>
+      <div>
+        <Route path="/:id" component={Child} />
+      </div>
+    </Router>
   );
+}
+
+function Child({ match }) {
+  const page = match.params.id ? parseInt(match.params.id) : 0;
+  console.log(page);
+  return <Carousel page={page} />;
 }
 
 export default App;
