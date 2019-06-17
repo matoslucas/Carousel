545b753bcdd41bdcd61ad296bfc70d5be285755c
Lucas Matos <matoslucas@hotmail.com>
2019-06-17 00:35:45 -0600

    add default route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  // Redirect
+} from "react-router-dom";
 
 import Carousel from "./com/Carousel";
 
 function App() {
   return (
     <Router>
-      <div>
+      <Switch>
+        <Route path="/" exact component={Child} />
         <Route path="/:id" component={Child} />
-      </div>
+      </Switch>
     </Router>
   );
 }
